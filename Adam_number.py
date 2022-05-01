5432ad9fde3b3f1ca4b40ebd3c32822d55f8a684
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-05-01 21:05:06 +0530

    n=int(input()) s=n*n sum1=0 sum2=0 while(n>0):     w=n%10     sum1=sum1*10+w     n=n//10 s1=sum1*sum1 while(s1>0):     a=s1%10     sum2=sum2*10+a     s1=s1//10 if(s==sum2):     print("True") else:     print("False")

---
+++ Adam_number.py
@@ -0,0 +1,17 @@
+n=int(input())
+s=n*n
+sum1=0
+sum2=0
+while(n>0):
+    w=n%10
+    sum1=sum1*10+w
+    n=n//10
+s1=sum1*sum1
+while(s1>0):
+    a=s1%10
+    sum2=sum2*10+a
+    s1=s1//10
+if(s==sum2):
+    print("True")
+else:
+    print("False")
