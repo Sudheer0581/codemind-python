6ee3c519548d2b4b08f49e89b911535dd4d5210f
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-05-19 15:35:54 +0530

    n=int(input()) arr=list(map(int,input().strip().split()))[:n] c=0 f=0 for i in range(n):     if(arr[i]%2!=0):         f+=1         if(i%2!=0):             c+=1 if(c==f):     print("True") else:     print("False")

---
+++ Strictly_ODD.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().strip().split()))[:n]
+c=0
+f=0
+for i in range(n):
+    if(arr[i]%2!=0):
+        f+=1
+        if(i%2!=0):
+            c+=1
+if(c==f):
+    print("True")
+else:
+    print("False")
