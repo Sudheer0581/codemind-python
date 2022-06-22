72edad1e6e622f48a1e77be7c9476067a45abe2a
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-06-22 23:31:20 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) l=[] for i in range(len(arr)//2):     l.append(arr[i])     l.append(arr[n-i-1]) if(n%2!=0):     l.append(arr[len(l)//2])     l.append(0) print(*l)

---
+++ First_and_Last_pair.py
@@ -0,0 +1,10 @@
+n=int(input())
+arr=list(map(int,input().strip().split()))
+l=[]
+for i in range(len(arr)//2):
+    l.append(arr[i])
+    l.append(arr[n-i-1])
+if(n%2!=0):
+    l.append(arr[len(l)//2])
+    l.append(0)
+print(*l)
