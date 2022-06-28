8a5b8d15ccee859a612e8f86c540fc726f8f8ff5
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-06-28 23:15:11 +0530

    n=int(input()) arr=list(map(int,input().strip().split()))[:n] #print(arr) l=[] d=0 for i in range(n):     p=arr[i]     if(p==0):         d+=1     if(p<0):         p=p*-1     while(p):         d+=1         p=p//10     l.append(d)     d=0 l.sort() #print(l) q=0 max=l[n-1] for i in range(n):     if(l[i]==max):         q+=1 print(q)

--- Max_digits_count.py
+++ Max_digits_count.py
@@ -1,15 +1,24 @@
 n=int(input())
 arr=list(map(int,input().strip().split()))[:n]
-m=0
-c=0
-s=0
-for i in arr:
-    p=str(i)
-    q=len(p)
-    if(q>=m):
-        m=q
-for i in arr:
-    q=str(i)
-    if(len(q)==m):
-        s+=1
-print(s)
+#print(arr)
+l=[]
+d=0
+for i in range(n):
+    p=arr[i]
+    if(p==0):
+        d+=1
+    if(p<0):
+        p=p*-1
+    while(p):
+        d+=1
+        p=p//10
+    l.append(d)
+    d=0
+l.sort()
+#print(l)
+q=0
+max=l[n-1]
+for i in range(n):
+    if(l[i]==max):
+        q+=1
+print(q)
