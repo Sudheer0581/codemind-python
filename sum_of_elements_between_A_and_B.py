ed3a5e66e4b26cd516bd52cf9235630935dc5658
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-06-29 15:08:28 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) #print(arr) l=[] a,b=map(int,input().split()) s=0 for i in range(n):     if(arr[i]>=a and arr[i]<=b):         l.append(arr[i])         s=s+arr[i] if(len(l)==0):     print("-1") else:     print(s)

--- sum_of_elements_between_A_and_B.py
+++ sum_of_elements_between_A_and_B.py
@@ -1,10 +1,14 @@
 n=int(input())
 arr=list(map(int,input().strip().split()))
-a,b=map(int,input().strip().split())
+#print(arr)
 l=[]
+a,b=map(int,input().split())
 s=0
-for i in range(a,b+1):
-    if(i in arr):
-        l.append(i)
-        s=s+i
-print(s)
+for i in range(n):
+    if(arr[i]>=a and arr[i]<=b):
+        l.append(arr[i])
+        s=s+arr[i]
+if(len(l)==0):
+    print("-1")
+else:
+    print(s)
