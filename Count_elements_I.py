6fd4b31c6fa2c1e6eb672c72ab010f7a3af27f03
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-06-29 14:18:51 +0530

    n,m=map(int,input().split()) arr=list(map(int,input().split()))[:n] l=list(map(int,input().split()))[:m] q1=set(arr) q2=set(l) c=0 if(n>=m):     for i in q1:         for j in q2:             if(i==j):                 #print(i)                 c+=1                 break else:     for i in q2:         for j in q1:             if(i==j):                 c+=1                 break print(c)

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,11 +1,20 @@
-a,b=map(int,input().split())
-arr1=list(map(int,input().strip().split()))
-arr2=list(map(int,input().strip().split()))
-l=[]
+n,m=map(int,input().split())
+arr=list(map(int,input().split()))[:n]
+l=list(map(int,input().split()))[:m]
+q1=set(arr)
+q2=set(l)
 c=0
-for i in range(len(arr1)):
-    for j in range(len(arr2)):
-        if(arr1[i]==arr2[j] and arr1[i] not in l):
-            l.append(arr1[i])
-            c+=1
+if(n>=m):
+    for i in q1:
+        for j in q2:
+            if(i==j):
+                #print(i)
+                c+=1
+                break
+else:
+    for i in q2:
+        for j in q1:
+            if(i==j):
+                c+=1
+                break
 print(c)
