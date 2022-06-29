7ae2aebf08f3c852bc6862335fb0133e05051d98
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-06-29 19:32:18 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) a,b=map(int,input().split()) l=[] s=0 for i in range(n):     if arr[i]<a or arr[i]>b:         l.append(arr[i]) print(sum(l))

--- sum_of_elements_not_between_A_and_B.py
+++ sum_of_elements_not_between_A_and_B.py
@@ -1,9 +1,9 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
-a,b=map(int,input().strip().split())
+arr=list(map(int,input().strip().split()))
+a,b=map(int,input().split())
+l=[]
 s=0
 for i in range(n):
-    if arr[i]>=a and arr[i]<=b:
-        continue
-    s=s+arr[i]
-print(s)
+    if arr[i]<a or arr[i]>b:
+        l.append(arr[i])
+print(sum(l))
