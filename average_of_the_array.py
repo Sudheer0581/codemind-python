7c9fc40a8da39e6da777f9929715baf346c6bbae
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-06-29 23:00:41 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) s=0 for i in arr:     s=s+i avg=s/n print("{:.2f}".format(avg))

--- average_of_the_array.py
+++ average_of_the_array.py
@@ -1,7 +1,7 @@
 n=int(input())
+arr=list(map(int,input().strip().split()))
 s=0
-a=list(map(int,input().strip().split()))
-for i in range(n):
-    s=s+a[i]
-c=s/n
-print("%.2f"%c)
+for i in arr:
+    s=s+i
+avg=s/n
+print("{:.2f}".format(avg))
