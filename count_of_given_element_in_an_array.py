800e71874b066c34588c5746bbde96edec6fa494
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-06-29 22:53:29 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) a=int(input()) c=0 for i in arr:     if(i==a):         c+=1 print(c)

--- count_of_given_element_in_an_array.py
+++ count_of_given_element_in_an_array.py
@@ -1,9 +1,8 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
+arr=list(map(int,input().strip().split()))
 a=int(input())
 c=0
-for i in range(n):
-    if(arr[i]==a):
+for i in arr:
+    if(i==a):
         c+=1
 print(c)
-   
