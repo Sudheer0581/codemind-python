b16dc6ab8b2d0c414c2645908eb8268cd947fd31
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-07-01 15:24:13 +0530

    n=input() s=n.split() l="".join(s) q=l.lower() print(len(set(q)))

--- Count_Distinct_characters.py
+++ Count_Distinct_characters.py
@@ -1,9 +1,5 @@
 n=input()
-n1=n.lower()
-s=''
-for i in n1:
-    if i not in s and ord(i)!=32:
-        s=s+i
-k=list(s)
-k.sort()
-print(len(k))
+s=n.split() 
+l="".join(s) 
+q=l.lower() 
+print(len(set(q)))
