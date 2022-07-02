542a6e19653e39c6aa57c0b57b573aa8e7275e04
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-07-02 16:54:23 +0530

    n=input() s=n.split() l='aeiouAEIOU' c=0 for i in s:     for j in i:         if j in l:             c+=1     print(c,end=' ')     c=0

--- vowel_count_in_each_word.py
+++ vowel_count_in_each_word.py
@@ -1,10 +1,10 @@
 n=input()
-q=n.split()
-l=['a','e','i','o','u']
+s=n.split()
+l='aeiouAEIOU'
 c=0
-for i in q:
+for i in s:
     for j in i:
-        if(j in l):
+        if j in l:
             c+=1
-    print(c,end=" ")
+    print(c,end=' ')
     c=0
