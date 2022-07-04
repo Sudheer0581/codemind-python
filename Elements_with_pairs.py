fe701de781d4510c7323bf3b465d06d0faceea96
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-07-04 21:18:59 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) if(n%2==0):     for i in arr:         print(i,end=" ") else:     arr.append(0)     for i in arr:         print(i,end=" ")

--- Elements_with_pairs.py
+++ Elements_with_pairs.py
@@ -1,11 +1,9 @@
 n=int(input())
-arr=list(map(int,input().strip().split()))[:n]
+arr=list(map(int,input().strip().split()))
 if(n%2==0):
     for i in arr:
         print(i,end=" ")
 else:
     arr.append(0)
     for i in arr:
-        print(i,end=' ')
-    
-    
+        print(i,end=" ")
