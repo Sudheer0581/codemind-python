d1249657ab0014cf2332430d76b3543351fb5410
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-07-04 21:34:20 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) #print(arr) l=[] for i in arr:     if i==arr.count(i):         l.append(i)         arr.remove(i) if(len(l)==0):     print("-1") else:     print(min(l),max(l))

--- Maximum_and_Minimum.py
+++ Maximum_and_Minimum.py
@@ -1,12 +1,12 @@
 n=int(input())
 arr=list(map(int,input().strip().split()))
+#print(arr)
 l=[]
-f=0
 for i in arr:
-    if i==arr.count(i) and i not in l:
+    if i==arr.count(i):
         l.append(i)
-        f=1
-if f==1:
-    print(min(l),max(l))
-else:
+        arr.remove(i)
+if(len(l)==0):
     print("-1")
+else:
+    print(min(l),max(l))
