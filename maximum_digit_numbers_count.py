f2c9fcf1aa2357f623072899a5bae3e3dc86a0c1
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-07-05 21:22:35 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) m=0 c=0 for i in arr:     if len(str(i))>m:         m=len(str(i)) for i in arr:     if(len(str(i))==m):         print(i,end=" ")

--- maximum_digit_numbers_count.py
+++ maximum_digit_numbers_count.py
@@ -1,15 +1,10 @@
-def count(n):
-    q=str(n)
-    return len(q)
 n=int(input())
 arr=list(map(int,input().strip().split()))
-max=0
+m=0
 c=0
 for i in arr:
-    b=count(i)
-    if(b>max):
-        max=b
+    if len(str(i))>m:
+        m=len(str(i))
 for i in arr:
-    if(len(str(i))==max):
+    if(len(str(i))==m):
         print(i,end=" ")
-    
