6f50be005c3d75095e8edac494be557df81ca0ad
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-07-06 21:32:10 +0530

    n=input() q=n.lower() l=[] for i in q:     if i.isalpha() and q.count(i)==1:         l.append(i) print(len(l))

--- count_unique_characters.py
+++ count_unique_characters.py
@@ -1,8 +1,7 @@
 n=input()
-a=n.lower()
-c=0
-for i in a:
-    if i!=' ':
-        if a.count(i)==1:
-            c+=1
-print(c)
+q=n.lower()
+l=[]
+for i in q:
+    if i.isalpha() and q.count(i)==1:
+        l.append(i)
+print(len(l))
