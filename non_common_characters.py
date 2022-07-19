0bdc05e18cddd603cfc7cc43c7243e5842eb4501
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-07-19 14:38:22 +0530

    n1=input() s1=n1.lower() n2=input() s2=n2.lower() l=[] v=[] k=' ' for i in s1:     if i not in s2 and i not in k and i not in l:         l.append(i) for i in s2:     if i not in s1 and i not in k and i not in l:         l.append(i) c=sorted(l) for i in c:     print(i,end='')

---
+++ non_common_characters.py
@@ -0,0 +1,18 @@
+n1=input()
+s1=n1.lower()
+n2=input()
+s2=n2.lower()
+l=[]
+v=[]
+k=' '
+for i in s1:
+    if i not in s2 and i not in k and i not in l:
+        l.append(i)
+for i in s2:
+    if i not in s1 and i not in k and i not in l:
+        l.append(i)
+c=sorted(l)
+for i in c:
+    print(i,end='')
+
+    
