f86b763de5fc3e8203786ce3b9825dd709331ec2
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-10-15 18:56:07 +0530

    a=input() b=input() r=a.lower() q=b.lower() x=set(r) z=set(q) q=list(x) v=list(z) c=0 for i in q:     if i in v:         v.remove(i)         c+=1 if(c==len(q)):     print("True") else:     print("False")

--- Anagrams.py
+++ Anagrams.py
@@ -8,18 +8,10 @@ q=list(x)
 v=list(z)
 c=0
 for i in q:
-    if(i in v):
+    if i in v:
         v.remove(i)
         c+=1
 if(c==len(q)):
     print("True")
 else:
     print("False")
-'''for i in l:
-    if i==1:
-        f=1
-        break
-if f==0 and a!=b:
-    print("True")
-else:
-    print("False")'''
