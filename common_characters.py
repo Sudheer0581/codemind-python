0f85b233c9b725fa904b959dbdb2ddb6ac7a1b7d
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-11-08 13:58:10 +0530

    n=input() n=n.lower() m=input() m=m.lower() str1="" str2="" l=[] for i in n:     str1=str1+i for i in m:     str2=str2+i for i in str1:     if i in str2:         l.append(i) k=set(l) z=list(k) q=(sorted(z)) if len(q)==0:     print("-1") else:     for i in q:         if i!=" ":             print(i,end='')

--- common_characters.py
+++ common_characters.py
@@ -1,26 +1,23 @@
-a=input()
-b=input()
-s=''
-x=''
-for i in a:
-    if i!=" ":
-        s+=i
-for i in b:
-    if i!=" ":
-        x+=i
-s=s.lower()
-x=x.lower()
-#print(s,x)
+n=input()
+n=n.lower()
+m=input()
+m=m.lower()
+str1=""
+str2=""
 l=[]
-for i in s:
-    if i in x and i not in l:
+for i in n:
+    str1=str1+i
+for i in m:
+    str2=str2+i
+for i in str1:
+    if i in str2:
         l.append(i)
-for i in x:
-    if i in s and i not in l:
-        l.append(i)
-v=sorted(l)
-if(len(l)==0):
+k=set(l)
+z=list(k)
+q=(sorted(z))
+if len(q)==0:
     print("-1")
 else:
-    for i in v:
-        print(i,end="")
+    for i in q:
+        if i!=" ":
+            print(i,end='')
