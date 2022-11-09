16218bd4851cb29e6343bc417f69d4ecf361ce05
Sudheer0581 <96948623+Sudheer0581@users.noreply.github.com>
2022-11-09 14:00:00 +0530

    n=input() n=n.lower() n=n.split() s1="" s2="" for i in n:     for j in i:         if j not in "aeiou":             s1=s1+j     k=sorted(s1)     x=0     s2=""     for c in i:         if c not in "aeiou":             s2=s2+k[x]             x=x+1         else:             s2=s2+c     print(s2,end=" ")     s1=

--- sort_consonents.py
+++ sort_consonents.py
@@ -1,20 +1,20 @@
-a=input()
-a=a.split()
-n=""
-for i in a:
-    k=0
-    x=""
-    i=str(i)
+n=input()
+n=n.lower()
+n=n.split()
+s1=""
+s2=""
+for i in n:
     for j in i:
         if j not in "aeiou":
-            x+=j
-    x=sorted(x)
-    for j in i:
-        if j in"aeiou":
-            n+=j
+            s1=s1+j
+    k=sorted(s1)
+    x=0
+    s2=""
+    for c in i:
+        if c not in "aeiou":
+            s2=s2+k[x]
+            x=x+1
         else:
-            n+=x[k]
-            k+=1
-    n+=" "
-n=n.split()
-print(*n)
+            s2=s2+c
+    print(s2,end=" ")       
+    s1=
